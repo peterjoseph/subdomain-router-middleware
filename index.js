2768a81f3e00be8c46c49f437ed71b968c2c986f
Peter Joseph <contact@peterajoseph.com>
2018-04-12 07:47:21 +0800

    Validate subdomains on production environments

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
+const options = {
+  asyncLoad: false,
+  error: {
+    success: false,
+    message: "Subdomain does not match available list"
+  }
+};
+
 const subdomain = {
-  // Define list of subdomains
-  init: function() {},
+  // Define options with single object
+  // Validate all keys in object
+  init: function() {
+    // Define asyncLoad object enabled
+    // Define asyncLoad subdomain list
+    // Define return failure object
+  },
 
   // Define subdomain route
   route: function(subdomain) {
     return function(req, res, next) {
-      if (req && req.hostname.match(/test\./g)) {
+      const url = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
+      const regex = /(?:http[s]*\:\/\/)*(.*?)\.(?=[^\/]*\..{2,4})/;
+      let route, i;
+      // Get the route from the url
+      if ((i = regex.exec(url)) !== null) {
+        i.forEach((match, groupIndex) => {
+          if (groupIndex == 1) {
+            route = match;
+          }
+        });
+      }
+      // If our subdomain is null, fetch list of required subdomains
+      if ((subdomain == null || subdomain == "") && options.asyncLoad == true) {
+        // Fetch our list of available subdomains
+      } else if (subdomain === route) {
         next();
+      } else {
+        res.status(403).json(options.error);
       }
     };
   }
